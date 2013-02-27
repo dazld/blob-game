9dbb5c7bb1d5bcc3c34f7583f2bed5eccd3c4fbf
Dan Peddle <dan.peddle@gmail.com>
2013-02-27 02:43:22 +0000

    draw

--- js/modules/stage.js
+++ js/modules/stage.js
@@ -10,10 +10,12 @@ define([
 		$el: false,
 		active: false,
 		topics: {
-
+			"window.resize":"render"
+			
 		},
 		events: {
-			"click":"doStuff"
+			"click":"doStuff",
+			"mousemove":"render"
 
 		},
 		init: function(){
@@ -22,7 +24,7 @@ define([
 			var holder = document.createElement(this.el);
 
 			var ctx = holder.getContext('2d');
-			this.sandbox.ctx = ctx;
+			this.sandbox.ctx = this.ctx = ctx;
 			// bind window resize events 
 			$(window).resize(function(e){
 				that.updateDimensions(e);
@@ -41,20 +43,40 @@ define([
 			var viewPortWidth = window.innerWidth,
    				viewPortHeight = window.innerHeight;
 
-			this.$el.width(viewPortWidth)
+			this.$el.attr('width',viewPortWidth)
+					.attr('height',viewPortHeight)
+					.width(viewPortWidth)
 					.height(viewPortHeight);
-		},66),
+
+			this.channel.publish('window.resize',{width: viewPortWidth,height: viewPortHeight});
+
+		},120),
 		start: function(){
 			this.active = true;
 		},
-		render: function(){
-
-		},
+		render: _.throttle(function(e){
+			var ctx = this.ctx;
+			if (!e) {
+				return;
+			};
+			ctx.beginPath();
+            ctx.moveTo(e.pageX,e.pageY);
+            ctx.lineCap = "round";
+            
+            ctx.strokeStyle = "rgba(64,64,64,0.5)";
+            ctx.lineWidth = 1;
+            
+            
+            
+            ctx.lineTo(10,10);
+            
+            ctx.stroke();
+		},20),
 		stop: function(){
 			this.active = false;	
 		},
 		doStuff: function(e){
-			console.log(this,e);
+			//console.log(this,e);
 		}
 	});
 
