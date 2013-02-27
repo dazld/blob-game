b7c1496a5df30ffe0ef7dff44356e16cf68165a5
Dan Peddle <dan.peddle@gmail.com>
2013-02-27 02:03:46 +0000

    resize

--- js/modules/stage.js
+++ js/modules/stage.js
@@ -17,19 +17,41 @@ define([
 
 		},
 		init: function(){
+			var that = this,
+				$w = $(window);
 			var holder = document.createElement(this.el);
+
+			var ctx = holder.getContext('2d');
+			this.sandbox.ctx = ctx;
+			// bind window resize events 
+			$(window).resize(function(e){
+				that.updateDimensions(e);
+			});
 
 			this.$el = $(holder);
 
-			this.$el.addClass('lala');
+			this.$el.addClass('stage');
 
 			this.bindEvents();
 
 			this.sandbox.root.append(this.$el);
 
 		},
-		start: function(){
+		updateDimensions: _.debounce(function(e){
+			var viewPortWidth = window.innerWidth,
+   				viewPortHeight = window.innerHeight;
 
+			this.$el.width(viewPortWidth)
+					.height(viewPortHeight);
+		},66),
+		start: function(){
+			this.active = true;
+		},
+		render: function(){
+
+		},
+		stop: function(){
+			this.active = false;	
 		},
 		doStuff: function(e){
 			console.log(this,e);
