c42d35f32b767d6ba5b0ab590c08d0ac023d0bcc
Dan Peddle <dan.peddle@gmail.com>
2013-03-18 00:34:32 +0000

    lines

--- js/modules/players.js
+++ js/modules/players.js
@@ -16,14 +16,37 @@ define([
 
 			var iterate = 0.5;
 
-			for (var i = 0; i < 1; i++) {
+			for (var i = 0; i < 150; i++) {
 				var bar = new PIXI.Sprite(this.sandbox.textures.bar);
+				var bx = position.x;
+				var by = position.y;
 
 				bar.anchor.x = 1;
 				bar.anchor.y = 1;
 				
-				bar.position.x = position.x;
-				bar.position.y = position.y;
+
+
+				switch(true){
+					case (i%2==0):
+						bx +=1;
+						by +=1;
+						break;
+					case (i%2==0):
+						bx -=1;
+						by -=1;
+						break;
+					case (i%3==0):
+						bx -=2;
+						by -=2;
+						break;
+					case (i%4==0):
+						bx +=2;
+						by +=2;
+						break;
+				}
+
+				bar.position.x = bx;
+				bar.position.y = by;
 				bar.scaler = 0.5;
 				bar.width = 1;
 				bar.height = 1;
